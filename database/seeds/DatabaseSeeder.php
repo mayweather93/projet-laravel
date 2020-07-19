30045b601d7af8913c8580c9dc008c87a27993ca
adam174 <admin@nachattube.com>
2020-07-19 11:58:17 +0200

    update seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
         $this->call(RoleUserTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        
      
      
      
