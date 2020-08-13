3aebae56778a3d8288f42a4182263f34ba8ac599
mayweather93 <nicocsl93@yahoo.fr>
2020-08-13 16:09:55 +0200

    init

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $faker->sentence(2),
-        'description' => $faker->sentence(20),
+        'description' => $faker->sentence(5),
         'price' => $faker->numberBetween(100, 5000),
 
     ];
